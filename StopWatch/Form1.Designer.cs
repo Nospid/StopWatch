e1c66b58675d7464f8a326b43bda93613cc1e727
nospid <dipsonpokhrel4@gmail.com>
2024-09-29 12:49:40 +0545

    Add project files.

---
+++ StopWatch/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace StopWatch
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.labelTime = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// labelTime
+			// 
+			this.labelTime.AutoSize = true;
+			this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelTime.Location = new System.Drawing.Point(154, 94);
+			this.labelTime.Name = "labelTime";
+			this.labelTime.Size = new System.Drawing.Size(173, 39);
+			this.labelTime.TabIndex = 0;
+			this.labelTime.Text = "\"00:00:00\"";
+			// 
+			// button1
+			// 
+			this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.button1.Location = new System.Drawing.Point(194, 159);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Start";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.button2.Location = new System.Drawing.Point(194, 201);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Stop";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.button3.Location = new System.Drawing.Point(194, 243);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 1;
+			this.button3.Text = "Reset";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.ClientSize = new System.Drawing.Size(467, 445);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.labelTime);
+			this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label labelTime;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ StopWatch/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StopWatch
+{
+	public partial class Form1 : Form
+	{
+		private int secondsElapsed; // Variable to store elapsed time in seconds
+		public Form1()
+		{
+			InitializeComponent();
+			secondsElapsed = 0;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			timer1.Start();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			timer1.Stop();
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+
+			timer1.Stop(); // Stop the timer
+			secondsElapsed = 0; // Reset elapsed time to zero
+			labelTime.Text = "00:00:00"; // Reset the label to 00:00:00
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			secondsElapsed++; // Increment the elapsed time by one second
+			TimeSpan time = TimeSpan.FromSeconds(secondsElapsed); // Convert seconds to TimeSpan
+			labelTime.Text = time.ToString(@"hh\:mm\:ss"); // Update label with formatted time (hours:minutes:seconds)
+		}
+	}
+}
